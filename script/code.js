2792a51baac9cfebbc7372364bece9eb4f28947a
AyabulelaSityata <sityataayabulela@gmail.com>
2023-05-24 16:35:44 +0200

    Question1

---
+++ script/code.js
@@ -0,0 +1,21 @@
+/*
+Question 1
+VARIABLE:
+Variables are containers for storing data (values), we use these three keyword types to delaclare a variable:
+i.e var, let or const
+
+e.g var x = 10;
+    var y = 15;
+
+VALUE:
+Is the output that is given when a variable is declared/assigned, it could be a string, boolean, number, object, symbol etc.
+
+e.g var x = 'Hello';
+    var y = 12;
+
+DATA TYPE:
+
+
+INITIALIZATION:
+
+One of the difference between let, var and const is that var is globally scoped meaning it can be
